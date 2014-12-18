ac0b7c575891be8cdfde690e5aba5d060e8a0039
Sean Warner <sintranti@gmail.com>
2014-12-17 21:44:14 -0600

    Added helpers to autosort and hilite sortable column

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,11 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def hilite(header)
+    header == params[:sort_by] ? 'hilite' : ''
+  end
+  def sortable_link(key)
+    title = key.to_s.gsub(/_/, ' ').split.map(&:capitalize).join(' ')
+    link_to title, movies_path(:sort_by => key), id: key.to_s + '_header'
+  end
 end
